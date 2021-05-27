57402e7e612ab9f46c97a5afd4489fbf6b3b7fc8
Rafif <hanifrafif22@gmail.com>
2021-05-27 12:17:25 +0700

    Modul 9 -TP

--- app/src/main/java/hanifzufarrafif/jwork_android/LoginActivity.java
+++ app/src/main/java/hanifzufarrafif/jwork_android/LoginActivity.java
@@ -10,6 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -25,16 +32,27 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    String email = etEmail.getText().toString();
-                    String password = etPassword.getText().toString();
-                    if (email.equals("test@test.com")&& password.equals("test")) {
-                        Toast.makeText(LoginActivity.this, "Login Succesful", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
-                    }
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
 
-                }
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if (jsonObject != null) {
+                                Toast.makeText(LoginActivity.this, "Register Successful", Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(LoginActivity.this, "Register Failed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+
         });
 
         tvRegister.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/hanifzufarrafif/jwork_android/LoginRequest.java
+++ app/src/main/java/hanifzufarrafif/jwork_android/LoginRequest.java
@@ -9,21 +9,19 @@ import java.util.Map;
 
 import static com.android.volley.Request.Method.POST;
 
-
-public class RegisterRequest extends StringRequest {
+public class LoginRequest extends StringRequest{
     private static final String URL = "http://192.168.100.10:8080/jobseeker/register";
     private Map<String, String> params;
 
-    public RegisterRequest(String name, String email, String password, Response.Listener<String> listener) {
+    public LoginRequest(String email, String password, Response.Listener<String> listener) {
         super(POST, URL, listener, null);
         params = new HashMap<>();
-        params.put("name", name);
         params.put("email", email);
         params.put("password", password);
     }
 
     @Override
-    protected Map<String, String> getParams() throws AuthFailureError{
+    protected Map<String, String> getParams() throws AuthFailureError {
         return params;
     }
 }

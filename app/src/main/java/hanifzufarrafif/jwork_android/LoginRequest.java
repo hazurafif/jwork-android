4b4112b508984d2bb6a491bb31f87ad3d535cf2c
Rafif <hanifrafif22@gmail.com>
2021-05-27 12:21:35 +0700

    Modul 9 - TP

--- app/src/main/java/hanifzufarrafif/jwork_android/LoginRequest.java
+++ app/src/main/java/hanifzufarrafif/jwork_android/LoginRequest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import static com.android.volley.Request.Method.POST;
 
 public class LoginRequest extends StringRequest{
-    private static final String URL = "http://192.168.100.10:8080/jobseeker/register";
+    private static final String URL = "http://192.168.100.10:8080/jobseeker/login";
     private Map<String, String> params;
 
     public LoginRequest(String email, String password, Response.Listener<String> listener) {
